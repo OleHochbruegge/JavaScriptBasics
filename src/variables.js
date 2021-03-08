9add4d950f97549e5ad526737fd4b1d7c33e6cb8
alikahwaji <ali.kahwaji@hotmail.com>
2021-03-09 02:59:13 +1300

    Added common errors in JS

---
+++ src/variables.js
@@ -0,0 +1,36 @@
+//TODO More variables explanation
+
+// Declare a variable with let ans assign a value to it
+let myName = 'Ali'
+
+// Try and a declare the same variable with a different value and print it.
+//let myName = 'John' // error 
+
+//Print it and check the result
+console.log(myName)
+/**
+ * the result will show an error:
+ * SyntaxError: Identifier 'myName' has already been declared.
+ * You can not declare a variable that you have already declared in same file.
+ * that's invalid in JavaScript.
+ * If you want to change a value for an existing variable.
+ */
+
+myName = 'Kahwaji'
+//Print it and check the result
+console.log(myName)
+
+// Declare a variable and assign a number to it
+// let 3 = 2 // error 
+//Print it and check the result
+
+// Use ! with a name to declare a variable
+let test! = 2 // error
+//Print it and check the result
+
+
+/**
+ * We can not use a number as variable name in JavaScript.
+ * We can not use ! as a variable name in JavaScript.
+ * You can not use JavaScript reserved keywords.
+ */
