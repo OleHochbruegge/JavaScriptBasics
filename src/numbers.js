55997734e6400f28b6976f4e29a139511ab98e5d
alikahwaji <ali.kahwaji@hotmail.com>
2021-03-09 02:28:45 +1300

    Added group operator

--- src/numbers.js
+++ src/numbers.js
@@ -1,6 +1,8 @@
 //TODO list examples of Numbers variables
 /**
  * Declaring a number variable is the same a string value except we do not need the '' when assigning a value.
+ * We can write any math operator on the same line of declaring a number variable.
+ * We can use grouping operator () to carry out a specific equation.
  */
 
 // Declaring a num variable with the value 503
@@ -8,27 +10,50 @@ let num = 503
 // Print it
 console.log(num)
 
-// We can use any of the math operators will declaring a number variable.
+// We can use any of the math operators will declaring a number variable. Use the Subtraction - operator.
 num = 503 - 3
 // Print it 
 console.log(num)
 
-// Using + operator 
+// Using the Addition  + operator 
 num = 500 + 3
 // Print it 
 console.log(num)
 
-// Using the / operator
+// Using the Division / operator
 num = 8 / 2
 // Print it 
 console.log(num)
 
-// Using the * operator
+// Using the Multiplication * operator
 num = 5 * 100
 // Print it 
 console.log(num)
 
-// Using the % Operator
+// Using the Remainder % Operator
 num = 9 % 2
 // Print it 
 console.log(num)
+
+// Using the Exponentiation ** operator
+num = 2 ** 3
+// Print it 
+console.log(num)
+
+// The example below is from MDN 
+const a = 1;
+const b = 2;
+const c = 3;
+
+// default precedence
+console.log(a + b * c)    // 7
+
+// evaluated by default like this
+console.log(a + (b * c))   // 7
+
+// now overriding precedence
+// addition before multiplication
+console.log((a + b) * c)   // 9
+
+// which is equivalent to
+console.log(a * c + b * c) // 9
