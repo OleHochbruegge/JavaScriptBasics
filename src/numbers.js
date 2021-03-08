e4c3340905f2599278e50987573b4e8bd8cf5bd0
alikahwaji <ali.kahwaji@hotmail.com>
2021-03-09 02:04:06 +1300

    Numbers values

---
+++ src/numbers.js
@@ -0,0 +1,34 @@
+//TODO list examples of Numbers variables
+/**
+ * Declaring a number variable is the same a string value except we do not need the '' when assigning a value.
+ */
+
+// Declaring a num variable with the value 503
+let num = 503
+// Print it
+console.log(num)
+
+// We can use any of the math operators will declaring a number variable.
+num = 503 - 3
+// Print it 
+console.log(num)
+
+// Using + operator 
+num = 500 + 3
+// Print it 
+console.log(num)
+
+// Using the / operator
+num = 8 / 2
+// Print it 
+console.log(num)
+
+// Using the * operator
+num = 5 * 100
+// Print it 
+console.log(num)
+
+// Using the % Operator
+num = 9 % 2
+// Print it 
+console.log(num)
